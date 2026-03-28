f0ddaefac74c2110e3df1465b8598789b1bee14d
pranavramesh2003 <cs22b015@smail.iitm.ac.in>
2026-03-28 08:50:51 +0530

    cleaned up lakefile.toml

--- LoVe/TransitionSystems.lean
+++ LoVe/TransitionSystems.lean
@@ -31,6 +31,7 @@ inductive trc {A : Type} (R : A → A → Prop) : A → A → Prop where
   -> trc R y z
   -> trc R x z
 
+
 /- Ironically, this definition is not obviously transitive!
   Let's prove transitivity as a lemma -/
 
@@ -45,7 +46,9 @@ trc R x y → forall z, (trc R y z → trc R x z) := by
     apply trc.TrcFront h x z ih h4
 
 
-theorem factorial_3 : trc fact_step (fact_state.WithAccumulator 3 1) (fact_state.AnswerIs 6)
+notation:max R "^*" => trc R
+
+theorem factorial_3 : fact_step ^* (fact_state.WithAccumulator 3 1) (fact_state.AnswerIs 6)
 := by
 eapply trc.TrcFront
 apply fact_step.FactStep
@@ -61,6 +64,40 @@ apply fact_step.FactDone
 eapply trc.TrcRefl
 
 
-theorem factorial_3_new : trc fact_step (fact_state.WithAccumulator 3 1) (fact_state.AnswerIs 6)
+theorem factorial_3_new : fact_step ^* (fact_state.WithAccumulator 3 1) (fact_state.AnswerIs 6)
 := by
 repeat econstructor
+
+structure trsys state where
+Initial : state → Prop
+Step : state → state → Prop
+
+def factorial_sys (original_input : ℕ) : trsys fact_state :=
+{
+  Initial := fact_init original_input
+  Step := fact_step
+}
+
+
+inductive reachable {state : Type} (sys : trsys state) (st : state) : Prop where
+| Reachable : forall st0,
+  sys.Initial st0
+  -> sys.Step ^* st0 st
+  -> reachable sys st
+
+/-
+
+**Invariant**
+   - What they are? [invariantFor]
+   - How to use them to prove transition systems? [use_invariant]
+   - How do you establish that an invariant holds for a
+     transition system? [invariant_induction]
+
+  To prove that our state machine is correct, we rely on
+   the crucial technique of *invariants*.
+
+   What is an invariant?
+
+   Here's a general definition, in terms of an arbitrary
+   transition system.
+-/
